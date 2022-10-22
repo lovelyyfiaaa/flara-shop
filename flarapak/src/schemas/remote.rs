1289fa5000aafa47e467f298ad5d6af9906533c5
Fiana Fortressia <fortressnordlys@outlook.com>
2022-10-23 06:06:08 +0700

    Implement parsing and types for flatpak repo configuration files
    Signed-off-by: Fiana Fortressia <fortressnordlys@outlook.com>

--- flarapak/src/schemas/remote.rs
+++ flarapak/src/schemas/remote.rs
@@ -1,65 +1,7 @@
-/**
- * [core]
-repo_version=1
-mode=bare-user-only
-min-free-space-size=500MB
-xa.pinned=runtime/org.fedoraproject.Platform/x86_64/f35;runtime/org.freedesktop.Sdk.Extension.llvm12/x86_64/21.08;runtime/org.freedesktop.Sdk.Extension.node14/x86_64/21.08;runtime/org.freedesktop.Sdk.Extension.rust-stable/x86_64/21.08;runtime/org.gnome.Platform/x86_64/master;runtime/org.gnome.Sdk/x86_64/42;runtime/org.gnome.Sdk/x86_64/master;runtime/org.gtk.Gtk3theme.adw-gtk3-dark/x86_64/3.22
-
-[remote "fedora"]
-url=oci+https://registry.fedoraproject.org
-gpg-verify=true
-gpg-verify-summary=true
-
-[remote "fedora-testing"]
-url=oci+https://registry.fedoraproject.org#testing
-xa.title=Fedora Flatpaks (testing)
-xa.title-is-set=true
-xa.disable=true
-
-[remote "flathub"]
-url=https://dl.flathub.org/repo/
-xa.title=Fedora Flathub Selection
-gpg-verify=true
-gpg-verify-summary=true
-xa.comment=Selected applications from Flathub
-xa.description=Selected applications from Flathub (https://flathub.org)
-xa.icon=https://dl.flathub.org/repo/logo.svg
-xa.homepage=https://flathub.org/
-xa.title-is-set=true
-xa.comment-is-set=true
-xa.description-is-set=true
-
-[remote "gnome-nightly"]
-url=https://nightly.gnome.org/repo/
-xa.title=GNOME Nightly
-gpg-verify=true
-gpg-verify-summary=true
-xa.description=The latest GNOME flatpak runtimes/apps directly from the gitlab CI. This is highly unstable stuff aimed at GNOME developers/designers to test
-xa.homepage=https://gnome.org/
-xa.gpg-keys-hash=18651a8e0151750586051c872d38670d30fbc7382ecf93d90a7bd4df6b9b803e
-
-[remote "flathub-beta"]
-url=https://dl.flathub.org/beta-repo/
-xa.title=Flathub beta
-gpg-verify=true
-gpg-verify-summary=true
-xa.comment=Beta builds of Flatpak applications
-xa.description=Beta builds of Flatpak applications
-xa.icon=https://dl.flathub.org/repo/logo.svg
-xa.homepage=https://flathub.org/
-
-[remote "devel-origin"]
-url=
-xa.title=pods.flatpak
-xa.noenumerate=true
-xa.prio=0
-gpg-verify-summary=false
-gpg-verify=false
-xa.main-ref=app/com.github.marhkb.Pods.Devel/x86_64/master
-
- */
+use configparser::ini::Ini;
+use serde::Serialize;
 use serde_derive::Deserialize;
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize, Debug, PartialEq, Eq, Ord, PartialOrd)]
 pub struct Remote {
     pub url: String,
     pub xa: Option<Xa>,
@@ -67,10 +9,101 @@ pub struct Remote {
     pub gpg_verify_summary: Option<bool>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize, Debug, PartialEq, Eq, Ord, PartialOrd)]
 pub struct Xa {
     pub prio: Option<usize>,
     pub noenumerate: Option<bool>,
     pub main_ref: Option<bool>,
-    pub pinned: Option<String>
+    pub pinned: Option<String>,
+    pub title: Option<String>,
+    pub description: Option<String>,
+    pub title_is_set: Option<bool>,
+    pub description_is_set: Option<bool>,
+    pub comment: Option<String>,
+    pub icon: Option<String>,
+    pub homepage: Option<String>,
+}
+pub fn deserialize(ini: Ini) -> Vec<Remote> {
+    let mut remotes = Vec::new();
+    for section in ini.sections() {
+        if section != "core" {
+            remotes.push(Remote {
+                url: ini.get(&section, "url").unwrap(),
+                xa: Some(Xa {
+                    title: ini.get(&section, "xa.title"),
+                    prio: ini
+                        .getuint(&section, "xa.prio")
+                        .unwrap()
+                        .map(|int| int as usize),
+                    noenumerate: ini.getbool(&section, "xa.noenumerate").unwrap(),
+                    main_ref: ini.getbool(&section, "xa.main-ref").unwrap(),
+                    pinned: ini.get(&section, "xa.pinned"),
+                    description: ini.get(&section, "xa.description"),
+                    title_is_set: ini.getbool(&section, "xa.title-is-set").unwrap(),
+                    description_is_set: ini.getbool(&section, "xa.description-is-set").unwrap(),
+                    comment: ini.get(&section, "xa.comment"),
+                    icon: ini.get(&section, "xa.icon"),
+                    homepage: ini.get(&section, "xa.homepage"),
+                }),
+                gpg_verify: ini.getbool(&section, "gpg-verify").unwrap(),
+                gpg_verify_summary: ini.getbool(&section, "gpg-verify-summary").unwrap(),
+            })
+        }
+    }
+
+    remotes
+}
+
+#[cfg(test)]
+mod test {
+    use configparser::ini::Ini;
+
+    use crate::schemas::remote::{Remote, Xa};
+
+    use super::deserialize;
+
+    #[test]
+    pub fn parse_remotes() {
+        let mut ini = Ini::new();
+        ini.read(include_str!("flatpak_repoconfig.ini").to_string())
+            .unwrap();
+
+        assert_eq!(
+            deserialize(ini),
+            vec![Remote {
+                url: "https://dl.flathub.org/repo/".to_string(),
+                xa: Some(Xa {
+                    prio: None,
+                    noenumerate: None,
+                    main_ref: None,
+                    pinned: None,
+                    title: Some("Fedora Flathub Selection".to_string()),
+                    description: Some(
+                        "Selected applications from Flathub (https://flathub.org)".to_string()
+                    ),
+                    title_is_set: Some(true),
+                    description_is_set: Some(true),
+                    comment: Some("Selected applications from Flathub".to_string()),
+                    icon: Some("https://dl.flathub.org/repo/logo.svg".to_string()),
+                    homepage: Some("https://flathub.org/".to_string())
+                }),
+                gpg_verify: Some(true),
+                gpg_verify_summary: Some(true),
+            }]
+        );
+
+        /*
+                [remote "flathub"]
+        url=https://dl.flathub.org/repo/
+        xa.title=Fedora Flathub Selection
+        gpg-verify=true
+        gpg-verify-summary=true
+        xa.comment=Selected applications from Flathub
+        xa.description=Selected applications from Flathub (https://flathub.org)
+        xa.icon=https://dl.flathub.org/repo/logo.svg
+        xa.homepage=https://flathub.org/
+        xa.title-is-set=true
+        xa.comment-is-set=true
+        xa.description-is-set=true */
+    }
 }
