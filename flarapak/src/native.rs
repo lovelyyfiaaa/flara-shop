186e8b7dc32414d0eff4d1c4e967deeb13a5a8bc
Fiana Fortressia <fortressnordlys@outlook.com>
2022-10-28 19:11:03 +0700

    flarapak: dynamic return type design on things
    Signed-off-by: Fiana Fortressia <fortressnordlys@outlook.com>

--- flarapak/src/native.rs
+++ flarapak/src/native.rs
@@ -3,31 +3,102 @@
 ///
 /// This requires a Linux-like environment in the program that has Flatpak installed in it. So WebAssembly is not supported by this one. You can try the flathub-beta-web backend however!!
 pub struct NativeBackend {}
+
+use std::fs::File;
+
+use std::io::Read;
+
 use std::path::PathBuf;
 
-use crate::prelude;
+use std::process::{Command, ExitStatus};
+
+use crate::schemas::appstream::Components;
 use crate::schemas::remote::{deserialize, Remote};
+use crate::{prelude, schemas};
 pub use NativeBackend as Backend;
+
+#[derive(Debug, Clone)]
 pub struct NativeRepository {
     remote: Remote,
+    path: PathBuf,
 }
 pub use NativeRepository as Repository;
 pub use Repository as Repo;
-impl prelude::Repository<App> for Repository {
-    fn get_apps() -> Vec<App> {
-        todo!()
+impl<'a> prelude::Repository<App> for Repository {
+    fn get_apps(&self) -> Vec<App> {
+        let path = self.path.join("appstream").join(&self.remote.name);
+
+        if !path.exists() {
+            Vec::new()
+        } else {
+            let arch = path
+                .join(std::env::consts::ARCH)
+                .join("active")
+                .join("appstream.xml");
+            let mut xml = String::new();
+
+            File::open(arch).unwrap().read_to_string(&mut xml).unwrap();
+            let components: Components = quick_xml::de::from_str(&xml).unwrap();
+
+            components
+                .components
+                .iter()
+                .map(|app| NativeApp { app: app.clone() })
+                .collect()
+        }
     }
 
-    fn name(&self) -> Option<String> {
+    fn name(&self) -> Option<&Self::StringRet> {
         if let Some(xa) = &self.remote.xa {
-            xa.title.clone()
+            xa.title.as_ref()
         } else {
             None
         }
     }
+
+    type StringRet = String;
 }
-pub struct NativeApp {}
-impl prelude::App for NativeApp {}
+
+#[derive(Debug, Clone)]
+pub struct NativeApp {
+    app: schemas::appstream::App,
+}
+use async_trait::async_trait;
+#[async_trait]
+impl prelude::App for NativeApp {
+    type StringRet = String;
+    fn id(&self) -> &Self::StringRet {
+        &self.app.id
+    }
+    fn title(&self) -> &Self::StringRet {
+        &self.app.name
+    }
+    async fn install(&self) -> std::io::Result<ExitStatus> {
+        Self::install_id(&self.app.id).await
+    }
+
+    async fn install_id(id: &str) -> std::io::Result<ExitStatus> {
+        let mut flatpak = Command::new("flatpak");
+        flatpak.args(["install", id]);
+
+        flatpak.status()
+    }
+    fn author(&self) -> Option<&Self::StringRet> {
+        self.app.developer_name.as_ref()
+    }
+
+
+
+    fn images(&self) -> Vec<&Self::StringRet> {
+        let mut images = Vec::new();
+
+        for screenshots in &self.app.screenshots.screenshots {
+            images.extend(&screenshots.images);
+        }
+
+        images.iter().map(|image| &image.url).collect()
+    }
+}
 use configparser::ini::Ini;
 pub use App as Application;
 pub use App as NativeApplication;
@@ -39,6 +110,8 @@ impl Backend {
 
 #[cfg(test)]
 mod test {
+    use std::path::PathBuf;
+
     use configparser::ini::Ini;
 
     use crate::native::Repo;
@@ -50,8 +123,11 @@ mod test {
         ini.read(include_str!("./schemas/flatpak_repoconfig.ini").to_string())
             .unwrap();
         for remote in deserialize(ini) {
-            let repo = Repo { remote };
-            assert_eq!(repo.name(), Some("Fedora Flathub Selection".to_string()));
+            let repo = Repo {
+                remote,
+                path: PathBuf::new(),
+            };
+            assert_eq!(repo.name(), Some(&"Fedora Flathub Selection".to_string()));
         }
     }
 }
@@ -68,20 +144,36 @@ impl prelude::Backend<App, Repo> for Backend {
             } else {
                 folder = PathBuf::from(str_folder)
             }
-            if let Ok(path) = folder.join("repo").join("config").canonicalize() {
+            if let Ok(path) = Self::repo_config(&folder) {
                 let mut ini = Ini::new();
                 ini.load(path).unwrap();
 
                 let remotes = deserialize(ini);
-                repositories.extend(remotes);
+                repositories.extend(remotes.iter().map(|remote| Repo {
+                    remote: remote.clone(),
+                    path: folder.clone(),
+                }));
             };
         }
 
         repositories
-            .iter()
-            .map(|remote| Repo {
-                remote: remote.clone(),
-            })
-            .collect()
+    }
+
+    fn get_apps(repos: &Vec<Repo>) -> Vec<App> {
+        let mut apps = Vec::new();
+
+        for repo in repos {
+            apps.extend(<NativeRepository as prelude::Repository<App>>::get_apps(
+                repo,
+            ))
+        }
+
+        apps
+    }
+}
+
+impl NativeBackend {
+    pub fn repo_config(folder: &PathBuf) -> std::io::Result<PathBuf> {
+        folder.join("repo").join("config").canonicalize()
     }
 }
--- flarapak/src/prelude.rs
+++ flarapak/src/prelude.rs
@@ -1,15 +1,44 @@
 ///
 /// A trait that
 ///
-pub trait Repository<A: App> {
-    fn get_apps() -> Vec<A>;
-    fn name(&self) -> Option<String>;
+pub trait Repository<A: App>
+where
+    Self::StringRet: Into<String>,
+{
+    type StringRet;
+    fn get_apps(&self) -> Vec<A>;
+    fn name(&self) -> Option<&Self::StringRet>;
 }
 
 pub trait Backend<A: App, R: Repository<A>> {
     fn get_repositories() -> Vec<R>;
+    fn get_apps(repos: &Vec<R>) -> Vec<A>;
 }
 
-/// A trait that represents all kinds of Flatpak Application!
-pub trait App {}
+use std::{
+    io,
+    process::{ExitCode, ExitStatus},
+};
 
+/// A trait that represents all kinds of Flatpak Application!
+///
+use async_trait::async_trait;
+#[async_trait]
+pub trait App
+where
+    Self::StringRet: Into<String>,
+{
+    type StringRet;
+
+    fn id(&self) -> &Self::StringRet;
+    fn author(&self) -> Option<&Self::StringRet>;
+
+S
+
+    fn images(&self) -> Vec<&Self::StringRet>;
+
+    fn title(&self) -> &Self::StringRet;
+
+    async fn install(&self) -> io::Result<ExitStatus>;
+    async fn install_id(id: &str) -> io::Result<ExitStatus>;
+}
